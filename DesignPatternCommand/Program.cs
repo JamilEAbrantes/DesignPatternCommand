b682147c04f37b8d792dd95eaee81c4e37eaab47
Jamil Efraim Abrantes <jamil.abrantes@quiver.local>
2019-12-03 15:25:45 -0300

    Add project files.

---
+++ DesignPatternCommand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
