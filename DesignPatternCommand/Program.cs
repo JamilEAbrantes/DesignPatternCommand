92556d0dd30f9b90b0a6a98c5c69573811bb2a52
Jamil Abrantes <34713030+JamilEAbrantes@users.noreply.github.com>
2019-12-03 15:35:15 -0300

    Update Program.cs

--- DesignPatternCommand/Program.cs
+++ DesignPatternCommand/Program.cs
@@ -1,12 +1,106 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
-namespace DesignPatternCommand
+namespace CommandDesignPattern05
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Sair_Com_O_Carro();
+            Console.WriteLine();
+            Estacionar_O_Carro();
+
+            Console.ReadKey();
+        }
+
+        private static void Sair_Com_O_Carro()
+        {
+            var comandos = new List<Action>
+            {
+                () => EletricaCommand.Status(),
+                () => EletricaCommand.LigarCarro(),
+                () => RotacaoCommand.VirarParaEsquerda(60),
+                () => MoverCommand.MoverParaFrente(4),
+                () => RotacaoCommand.NivelarDirecao(),
+                () => MoverCommand.MoverParaFrente(100)
+            };
+
+            Executar.ExecutarComandos(comandos);
+        }
+
+        private static void Estacionar_O_Carro()
+        {
+            var comandos = new List<Action>
+            {
+                () => RotacaoCommand.VirarParaDireita(70),
+                () => MoverCommand.MoverParaFrente(5),
+                () => RotacaoCommand.NivelarDirecao(),
+                () => MoverCommand.MoverParaTras(2),
+                () => EletricaCommand.DesligarCarro(),
+                () => EletricaCommand.Status()
+            };
+
+            Executar.ExecutarComandos(comandos);
         }
     }
+
+    #region --> Commandos
+
+    public static class Executar
+    {
+        public static void ExecutarComandos(IEnumerable<Action> comandos)
+        {
+            foreach (var comando in comandos)
+                comando();
+        }
+
+        public static void DesfazerComandos(IEnumerable<Action> comandos)
+        {
+            foreach (var comando in comandos)
+                comando();
+        }
+    }
+
+    public static class EletricaCommand
+    {
+        private static bool Ligado { get; set; }
+
+        public static void LigarCarro()
+            => Console.WriteLine($"Ligar carro: { Ligado = true }.");
+
+        public static void DesligarCarro()
+            => Console.WriteLine($"Desligar carro: { Ligado = false }.");
+
+        public static void Status()
+        {
+            if (Ligado)
+                Console.WriteLine($"Veículo ligado.");
+            else
+                Console.WriteLine($"Veículo desligado.");
+        }
+    }
+
+    public static class RotacaoCommand
+    {
+        public static void VirarParaDireita(double graus)
+            => Console.WriteLine($"Virar p/ direita: { graus } graus.");
+
+        public static void VirarParaEsquerda(double graus)
+            => Console.WriteLine($"Virar p/ esquerda: { graus } graus.");
+
+        public static void NivelarDirecao()
+            => Console.WriteLine($"Direção nivelada: 0 graus.");
+    }
+
+    public static class MoverCommand
+    {
+        public static void MoverParaFrente(double metros)
+            => Console.WriteLine($"Mover p/ frente: { metros } metros.");
+
+        public static void MoverParaTras(double metros)
+            => Console.WriteLine($"Mover p/ tras: { metros } metros.");
+    }
+
+    #endregion
 }
